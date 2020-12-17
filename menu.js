aaf4be04798c8b78c2c6384bf0e84cd4c3412be4
GD2 Project 3 <none>
2020-12-17 20:50:55 +0000

    📉🛢 Checkpoint ./styles.css:21883344/37 ./menu.js:21883344/2312

--- menu.js
+++ menu.js
@@ -1,10 +1,10 @@
+
 $(".js-toggle-trigger").attr("aria-expanded", false).attr("aria-label", "Toggle site menu");
 
 
 $(".js-toggle-trigger").click(function () {
-
+  
   let expanded = $(this).attr("aria-expanded") === "true" || false;
-
+  
   $(this).attr("aria-expanded", !expanded);
 });
- 
